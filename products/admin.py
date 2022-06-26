9b1ca39ebd00474960e42544b98fcd93b470d99c
legenduzair <uzairoomer@live.com>
2022-06-26 00:41:16 +0000

    Acode for product models and admin python files

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Product, Category
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+class ProductAdmin(SummernoteModelAdmin):
+    summernote_fields = ('description', 'additional_information',)
+    list_display = (
+        'sku',
+        'name',
+        'meat_category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
