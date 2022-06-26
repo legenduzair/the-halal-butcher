d9da17f318d634a6f8e958020871c8d11c434566
legenduzair <uzairoomer@live.com>
2022-06-26 19:47:34 +0000

    Added sorting functionality by inputting relevant code in views.py

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Product, Category
 # Create your views here.
 
@@ -10,27 +11,49 @@ def product_list(request):
     products = Product.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
 
-    if 'category' in request.GET:
-        categories = request.GET['category'].split(',')
-        products = products.filter(meat_category__name__in=categories)
-        categories = Category.objects.filter(name__in=categories)
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            
+            if sortkey == 'category':
+                sortkey = 'category__name'
 
-    if 'q' in request.GET:
-        query = request.GET['q']
-        if not query:
-            messages.error(request, "You didn't enter any search criteria!")
-            return redirect(reverse('products'))
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
 
-        queries = (Q(name__icontains=query) | 
-                   Q(description__icontains=query) |
-                   Q(additional_information__icontains=query))
-        products = products.filter(queries)
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(meat_category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+
+            queries = (Q(name__icontains=query) |
+                       Q(description__icontains=query) |
+                       Q(additional_information__icontains=query))
+            products = products.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
         'search_query': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products.html', context)
