37cfb459a96faf6d9e8215345f46fc79bb857227
legenduzair <uzairoomer@live.com>
2022-06-26 20:13:28 +0000

    Added sorting box and the code required for the functionality of it to products.html

--- products/views.py
+++ products/views.py
@@ -21,9 +21,6 @@ def product_list(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-            
-            if sortkey == 'category':
-                sortkey = 'category__name'
 
             if 'direction' in request.GET:
                 direction = request.GET['direction']
