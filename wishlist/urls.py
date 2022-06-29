3d30af30a5a3636fb7bdfc246595a7097bcd30b0
legenduzair <uzairoomer@live.com>
2022-06-29 22:40:16 +0000

    Add remove from wishlist view in views.py and add urls

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.view_wishlist, name='wishlist'),
     path('add_to_wishlist/<int:item_id>', views.add_to_wishlist, name='add_to_wishlist'),
+    path('remove_from_wishlist/<int:item_id>', views.remove_from_wishlist, name='remove_from_wishlist'),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, reverse
 from products.models import Product
 
 from django.contrib import messages
@@ -33,3 +33,15 @@ def add_to_wishlist(request, item_id):
     messages.info(request, f'{product.name} was added to your wishlist')
 
     return redirect(redirect_url)
+
+
+def remove_from_wishlist(request, item_id):
+ 
+    wish = Wishlist.objects.get(user=request.user)
+    product = get_object_or_404(Product, pk=item_id)
+
+    # Remove wish from the wishlist
+    wish.products.remove(product)
+    messages.info(request, f'{product.name} was removed from your wishlist')
+
+    return redirect(reverse('wishlist'))
