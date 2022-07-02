7823d84d0ccc9e9c6bb8c84539e05748e70696cd
legenduzair <uzairoomer@live.com>
2022-07-02 17:14:14 +0000

    Added js and python code to implement the initiation of checkout functionality

--- checkout/views.py
+++ checkout/views.py
@@ -1,24 +1,46 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from basket.contexts import basket_contents
+
+import stripe
 
 # Create your views here.
 
 
 def checkout(request):
 
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     basket = request.session.get('basket', {})
     if not basket:
         messages.error(request, 'There are no items in your basket at the moment.')
         return redirect(reverse('products'))
 
+    current_basket = basket_contents(request)
+    total = current_basket['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    print(intent)
+
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stipe public key is missing, set in env variables')
+
     template = 'checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51L7Jk3EEYiOGw3IXl1FXLtlRCAPQJvx2HdLxSY2zbDc3mYxjGJYMI1K9DA0CYWhgzfyj9xnedrHPNRBbWxEVDf5H007NayRIHc',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
