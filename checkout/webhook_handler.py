a685e839a804901932f84bed7f29727ed91188d5
legenduzair <uzairoomer@live.com>
2022-07-03 15:04:07 +0000

    Edit form details in webhook handler file according to the databse models

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -37,14 +37,14 @@ class StripeWH_Handler:
         while attempt <= 5:
             try:
                 order = Order.objects.get(
-                    full_name__iexact=shipping_details.name,
+                    first_name__iexact=shipping_details.first_name,
+                    last_name__iexact=shipping_details.last_name,
                     email__iexact=billing_details.email,
                     phone_number__iexact=shipping_details.phone,
                     country__iexact=shipping_details.address.country,
                     postcode__iexact=shipping_details.address.postal_code,
                     town_or_city__iexact=shipping_details.address.city,
-                    street_address_1__iexact=shipping_details.address.line1,
-                    street_address_2__iexact=shipping_details.address.line2,
+                    street_address__iexact=shipping_details.address.line1,
                     county__iexact=shipping_details.address.state,
                     grand_total=grand_total,
                     original_bag=bag,
@@ -64,14 +64,14 @@ class StripeWH_Handler:
             order = None
             try:
                 order = Order.objects.create(
-                    full_name=shipping_details.name,
+                    first_name=shipping_details.first_name,
+                    last_name=shipping_details.last_name,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,
                     postcode=shipping_details.address.postal_code,
                     town_or_city=shipping_details.address.city,
-                    street_address_1=shipping_details.address.line1,
-                    street_address_2=shipping_details.address.line2,
+                    street_address=shipping_details.address.line1,
                     county=shipping_details.address.state,
                     original_bag=bag,
                     stripe_pid=pid,
