1fcdf302f5b5620bd1aea13002e63db8d5487f80
legenduzair <uzairoomer@live.com>
2022-07-11 21:55:51 +0000

    Add Javascript logic to alter color of country field, add more to profile views and templates

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import Profile
 from .forms import ProfileForm
@@ -8,12 +9,23 @@ def profile(request):
 
     profile = get_object_or_404(Profile, user=request.user)
     form = ProfileForm(request.POST, instance=profile)
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request, 'Update failed. \
+                Please ensure the form is correct')
+    else:
+        form = ProfileForm(instance=profile)
     
     orders = profile.orders.all()
     template = 'profiles.html'
     context = {
         'form': form,
         'orders': orders,
+        'on_profile_page': True
     }
 
     return render(request, template, context)
