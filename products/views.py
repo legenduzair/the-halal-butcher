8a3b26319f4248c9f5863f907290edb066cd0e7a
legenduzair <uzairoomer@live.com>
2022-07-13 21:23:45 +0000

    Add delete product view and add edit delete buttons to product and product detail html templates

--- products/views.py
+++ products/views.py
@@ -73,9 +73,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Product added successfully')
-            return redirect('add_product')
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'There is an error in the form.')
     else:
@@ -114,3 +114,15 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+  
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+        
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted successfully')
+    return redirect(reverse('products'))
