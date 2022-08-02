3a5912169c771aa6dcd95d27a0c5471d772f3059
legenduzair <uzairoomer@live.com>
2022-08-02 20:51:08 +0000

    Add summernote to blog admin, add first blog post and rearrange css

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from .models import BlogPost
+from django_summernote.admin import SummernoteModelAdmin
 
 
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(SummernoteModelAdmin):
     list_display = ('title', 'slug', 'created_on')
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
 
 
 admin.site.register(BlogPost, PostAdmin)
