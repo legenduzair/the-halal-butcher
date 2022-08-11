5ef6c1c1588f53dcc815eeabd9b836ef9d3186b6
legenduzair <uzairoomer@live.com>
2022-08-11 22:01:31 +0000

    Temprorarily remove error handler 500

--- halal_butcher/views.py
+++ halal_butcher/views.py
@@ -6,9 +6,9 @@ def handler404(request, exception):
     return render(request, "errors/404.html", status=404)
 
 
-def handler500(request, *args, **argv):
-    """ Error Handler 500 - Internal Server Error """
-    return render(request, "errors/500.html", status=500)
+# def handler500(request, *args, **argv):
+#     """ Error Handler 500 - Internal Server Error """
+#     return render(request, "errors/500.html", status=500)
 
 
 def handler403(request, exception):
