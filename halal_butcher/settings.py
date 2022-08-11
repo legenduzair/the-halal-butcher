dc93a31704f93c591e82a0fab2fac6d1b28d18d6
legenduzair <uzairoomer@live.com>
2022-08-11 22:50:37 +0000

    Remove server email variable

--- halal_butcher/settings.py
+++ halal_butcher/settings.py
@@ -228,7 +228,6 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'thehalalbutcher@example.com'
-    SERVER_EMAIL = 'thehalalbutcher@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_HOST = 'smtp.gmail.com'
@@ -237,7 +236,6 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
-    SERVER_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 
 # Default primary key field type
